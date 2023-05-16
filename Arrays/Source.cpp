02c838d1bbb14fa28d8eaf435e44859fc8aa3d4e
Екатерина Бахтурова < @LAPTOP-K6JU5RAN>
2023-05-16 14:00:12 +0300

    SHIFT_DONE

--- Arrays/Source.cpp
+++ Arrays/Source.cpp
@@ -3,26 +3,27 @@
 using namespace std;
 
 void main()
+
 {
-
 	setlocale(LC_ALL, "");
-
+#define ARRAYS
+#ifdef ARRAYS
 	/*const int SIZE = 5;
-	int arr[SIZE];
-	arr[2] = 123;
-	cout << arr[2]  << endl;
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << arr [i] << "\t";
-	}
-	cout << endl;*/
+int arr[SIZE];
+arr[2] = 123;
+cout << arr[2]  << endl;
+for (int i = 0; i < SIZE; i++)
+{
+	cout << arr [i] << "\t";
+}
+cout << endl;*/
 	const int SIZE = 5;
 	int arr[SIZE];
 	for (int i = 0; i < SIZE; i++)              //запись с клавиатуры
 	{
 		int n;
-		cout << "\t" << "Введите элемент  " << i << "\t"; cin >> n;
-		arr[i] = n;
+		cout << "\t" << "Введите элемент  " << i << "\t"; cin >> arr[i];
+		//arr[i] = n;
 	}
 	cout << endl << endl;
 	for (int i = 0; i < SIZE; i++)              //вывод элементов горизонтально от 0 до последнего
@@ -35,15 +36,16 @@ void main()
 		cout << "\t" << arr[i] << "\t";
 	}
 	cout << endl << endl;
-	int Summa = arr[0];                         //вывод сумму элементов массива
+	int Summa = 0;                         //вывод сумму элементов массива
 	for (int i = 0; i < SIZE; i++)
 	{
-		Summa = Summa + arr[i];
+		Summa += arr[i];
+		//Summa = Summa + arr[i];
 	}
 	cout << "Сумма элементов массива = " << Summa;
 	cout << endl;
 	cout << endl;
-	int Sredne_Arifmeticheskoe = Summa / SIZE;  //вывод средне-арифметического элементов массива
+	double Sredne_Arifmeticheskoe = (double)Summa / SIZE;  //вывод средне-арифметического элементов массива
 	cout << "Среднее-арифметическое элементов массива = " << Sredne_Arifmeticheskoe;
 	cout << endl << endl;
 	int Max = arr[0];                            //максимальное значение в массиве;
@@ -52,7 +54,7 @@ void main()
 		if (arr[i] > Max)
 		{
 			Max = arr[i];
-			
+
 		}
 	}
 	cout << "Максимальное значение в массиве = " << Max;
@@ -63,9 +65,9 @@ void main()
 		if (arr[i] < Min)
 		{
 			Min = arr[i];
-
 		}
 	}
 	cout << "Минимальное значение в массиве = " << Min;
 	cout << endl;
+#endif
 }
